9eb0c38e23ad65bf16de31d63693b5e242793770
Bharanilikith <Bharanilikith@Bharani>
2022-10-26 18:04:14 +0530

    DDT

---
+++ src/datadriven/Test1.java
@@ -0,0 +1,37 @@
+package datadriven;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+	public static void main(String[] args) throws EncryptedDocumentException, IOException {
+
+FileInputStream fis=new FileInputStream("./Book1.xlsx")	;
+Workbook wb = WorkbookFactory.create(fis);
+String url = wb.getSheet("Sheet1").getRow(1).getCell(0).getStringCellValue();
+String user = wb.getSheet("Sheet1").getRow(1).getCell(1).getStringCellValue();
+String pwd = wb.getSheet("Sheet1").getRow(1).getCell(2).getStringCellValue();
+	
+WebDriver driver=new ChromeDriver();
+driver.manage().window().maximize();
+driver.get(url);
+driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+driver.findElement(By.id("username")).sendKeys(user);
+driver.findElement(By.name("pwd")).sendKeys(pwd);
+
+
+
+	
+	}
+
+}
